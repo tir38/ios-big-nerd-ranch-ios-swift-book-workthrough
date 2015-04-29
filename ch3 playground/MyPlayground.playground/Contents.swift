39054f30635985c194de4f706b6d3dba23b5f236
Jason Atwood <jatwood@bignerdranch.com>
2015-04-28 22:02:49 -0400

    finishes ch3

---
+++ ch3 playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 1200
+let townName: String = "Atlanta"
+
+var unemploymentRatePercent: Float = 12.3
+
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights. Unemployment is currently at \(unemploymentRatePercent) percent"
