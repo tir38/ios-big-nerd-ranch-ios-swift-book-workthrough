e6b0809af98cdb51a716523a07b192401be9d6e6
Jason Atwood <jatwood@bignerdranch.com>
2015-05-04 09:32:24 -0400

    Initial Commit

---
+++ quiz_app/Quiz/QuizTests/QuizTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by Jason Atwood on 5/4/15.
+//  Copyright (c) 2015 Jason Atwood. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class QuizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
