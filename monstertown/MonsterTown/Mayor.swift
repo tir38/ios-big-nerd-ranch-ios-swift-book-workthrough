6d990a9ce16c859a7fe3e598ad99c8c3f34c4529
Jason Atwood <jatwood@bignerdranch.com>
2015-05-03 20:51:10 -0400

    finishes ch16 gold challenge

--- monstertown/MonsterTown/Mayor.swift
+++ monstertown/MonsterTown/Mayor.swift
@@ -2,9 +2,14 @@ import Foundation
 
 
 struct Mayor {
+ 
+    private var anxietyLevel:Int = 0
     
-    func notifiyOfPopulationChange(populationChange : Int ) {
+    mutating func notifiyOfPopulationChange(populationChange : Int ) {
         if populationChange < 0 {
+            
+            anxietyLevel++
+            
             println("I'm deeply saddened to hear about this latest tragedy of \(-1 * populationChange) deaths at the hands of the monsters. I promise that my office is looking into the nature of this rash of violence.")
         }
     }
