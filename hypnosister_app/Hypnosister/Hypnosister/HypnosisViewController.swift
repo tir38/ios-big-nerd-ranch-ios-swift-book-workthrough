774ba8dbb8d718152da21a90278273c28acbf304
Jason Atwood <jatwood@bignerdranch.com>
2015-05-05 15:30:13 -0400

    starts ios ch6gs

--- hypnosister_app/Hypnosister/Hypnosister/HypnosisViewController.swift
+++ hypnosister_app/Hypnosister/Hypnosister/HypnosisViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class HypnosisViewController: UIViewController {
 
+    weak private var textLabel: UILabel!
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
@@ -23,9 +25,37 @@ class HypnosisViewController: UIViewController {
         
         // set the background as *the (one and only)* view for this controller
         view = backgroundView
+        
+        // setup the label
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        label.center = view.center
+        label.font = UIFont.systemFontOfSize(24)
+        label.textAlignment = .Center
+        label.text = "Bill\nFreakout"
+        label.numberOfLines = 2
+        view.addSubview(label)
+        textLabel = label
     }
     
     override func viewDidLoad() {
         println("hypnosis view loaded")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+            
+        UIView.animateWithDuration(0.5,
+            delay: 0.0,
+            options: .Autoreverse | .Repeat,
+            animations: {
+                self.textLabel.alpha = 1.0
+            },
+            completion: nil)
     }
 }
